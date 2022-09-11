a60be4c195d7c3e28d6b380d5c351b752f30ce5f
Hritik Rajput <90711777+hritik7818@users.noreply.github.com>
2022-09-11 19:20:11 +0530

    working component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+import WeekyExpendeture from "./WeeklyExpendeture/WeekyExpendeture";
+
+const App = () => {
+  return (
+    <>
+      <WeekyExpendeture />
+    </>
+  );
+};
+
+export default App;
